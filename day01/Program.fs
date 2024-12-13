51e192ad39449b833513a0b8c3bc6a67eb981683
Kaspar Moberg <kaspeti123@gmail.com>
2024-12-13 09:45:24 +0100

    feat(fhsarp): did day01 task01

---
+++ day01/Program.fs
@@ -0,0 +1,23 @@
+﻿open System
+open System.IO
+
+let getColumns filePath = 
+    File.ReadLines(filePath)
+    |> Seq.fold (fun (col1, col2) line ->
+        let split = line.Split([|' '|], StringSplitOptions.RemoveEmptyEntries)
+        match split with
+        | [|first; second|] -> 
+            match Int32.TryParse first, Int32.TryParse second with
+            | ((true, n1), (true, n2)) -> (n1 :: col1, n2 :: col2)
+            | _ -> (col1, col2)
+        | _ -> (col1, col2)
+    ) ([], [])
+    |> fun (col1, col2) -> (List.rev col1, List.rev col2)
+
+
+let main = 
+    getColumns "input"
+    |> fun(col1, col2) -> (List.sort(col1), List.sort(col2))
+    |> fun(col1, col2) -> List.map2 (fun n1 n2 -> abs(n1 - n2)) col1 col2
+    |> List.sum
+    |> printfn "%d"
